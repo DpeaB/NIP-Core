ea7f57d9580ce4e4e359591d341496ecbafd7b9b
DpeaB <hunterhull4e@gmail.com>
2026-05-07 16:23:52 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Core command system for program wide operations and management.",
         // Activity type number (0 = Playing).
         type: 0, 
       },
